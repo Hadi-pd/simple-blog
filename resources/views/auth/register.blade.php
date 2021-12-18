51adc5705de8f01e91a22a57443daf6cd109e4cf
Hadi-pd <h7ad3i@chmail.ir>
2021-12-18 18:52:02 +0330

    Add persian validation

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -9,25 +9,69 @@
             <div class="sign-page">
                 <h1 class="sign-page__title">ثبت نام در وب‌سایت</h1>
                 
-                @foreach($errors->all() as $error)
+                {{-- @foreach($errors->all() as $error)
                 <p>{{ $error }}</p>
-                @endforeach
+                @endforeach --}}
 
-                <form class="sign-page__form" action=" {{ route('register.store') }}" method="POST">
+                <form class="sign-page__form" action="{{ route('register.store') }}" method="POST">
                     @csrf
-                    <input type="text" name="name" class="text text--right" placeholder="نام و نام خانوادگی" />
-                    <input type="text" name="mobile" class="text text--left" placeholder="شماره موبایل" />
-                    <input type="text" name="email" class="text text--left" placeholder="ایمیل" />
-                    <input type="password" name="password" class="text text--left" placeholder="رمز عبور" />
-                    <input type="password" name="password_confirmation" class="text text--left" placeholder="تکرار رمز عبور" />
+                    <div>
+                        <input type="text" name="name" class="text text--right" placeholder="نام و نام خانوادگی" />
+                        @error('name')
+                            <p style="margin-bottom: 1rem;
+                                    color: #D8000C;
+                                    text-align: right;"
+                            >
+                                {{ $message }}
+                            </p>
+                        @enderror
+                    </div>
     
+                    <div>
+                        <input type="text" name="mobile" class="text text--left" placeholder="شماره موبایل" />
+                        @error('mobile')
+                            <p style="margin-bottom: 1rem;
+                                    color: #D8000C;
+                                    text-align: right;"
+                            >
+                                {{ $message }}
+                            </p>
+                        @enderror
+                    </div>
+                    <div>
+                        <input type="text" name="email" class="text text--left" placeholder="ایمیل" />
+                        @error('email')
+                            <p style="margin-bottom: 1rem;
+                                    color: #D8000C;
+                                    text-align: right;"
+                            >
+                                {{ $message }}
+                            </p>
+                        @enderror
+                    </div>
+                    <div>
+                        <input type="password" name="password" class="text text--left" placeholder="رمز عبور" />
+                        @error('password')
+                            <p style="margin-bottom: 1rem;
+                                    color: #D8000C;
+                                    text-align: right;"
+                            >
+                                {{ $message }}
+                            </p>
+                        @enderror
+                    </div>
+                    <div>
+                        <input type="password" name="password_confirmation" class="text text--left" placeholder="تکرار رمز عبور" />
+                    </div>
     
-                        <button type="submit" class="btn btn--blue btn--shadow-blue width-100 mb-10">ثبت نام</button>
-                        <button type="reset" class="btn btn--red btn--shadow-red width-100 ">پاک کردن</button>
-                        <div class="sign-page__footer">
-                            <span>در سایت عضوید ؟ </span>
-                            <a href="{{ route('login') }}" class="color--46b2f0">صفحه ورود</a>
-                        </div>
+                    <button class="btn btn--blue btn--shadow-blue width-100 mb-10" type="submit">
+                        ثبت نام
+                    </button>
+                    <div class="sign-page__footer">
+                        <span>در سایت عضوید ؟ </span>
+                        <a href="{{ route('login') }}" class="color--46b2f0">صفحه ورود</a>
+    
+                    </div>
                 </form>
             </div>
         </div>
