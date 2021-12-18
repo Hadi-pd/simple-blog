0e43eed68ff838a54921c06317a7a2d2f92ec872
Hadi-pd <h7ad3i@chmail.ir>
2021-12-18 18:07:57 +0330

    Add user register

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -34,13 +34,15 @@ class RegisteredUserController extends Controller
     public function store(Request $request)
     {
         $request->validate([
-            'name' => ['required', 'string', 'max:255'],
-            'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
-            'password' => ['required', 'confirmed', Rules\Password::defaults()],
+            'name' => 'required|string|max:255',
+            'mobile' => 'required|string|unique:users|max:255',
+            'email' => 'required|string|email|max:255|unique:users',
+            'password' => 'required|string|confirmed|min:8',
         ]);
 
         $user = User::create([
             'name' => $request->name,
+            'mobile' => $request->mobile,
             'email' => $request->email,
             'password' => Hash::make($request->password),
         ]);
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,59 +1,35 @@
-<x-guest-layout>
-    <x-auth-card>
-        <x-slot name="logo">
-            <a href="/">
-                <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
-            </a>
-        </x-slot>
+<x-app-layout>
 
-        <!-- Validation Errors -->
-        <x-auth-validation-errors class="mb-4" :errors="$errors" />
+    <x-slot name="title">
+        - صفحه ثبت نام
+    </x-slot>
+    
+    <main class="bg--white">
+        <div class="container">
+            <div class="sign-page">
+                <h1 class="sign-page__title">ثبت نام در وب‌سایت</h1>
+                
+                @foreach($errors->all() as $error)
+                <p>{{ $error }}</p>
+                @endforeach
 
-        <form method="POST" action="{{ route('register') }}">
-            @csrf
-
-            <!-- Name -->
-            <div>
-                <x-label for="name" :value="__('Name')" />
-
-                <x-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required autofocus />
+                <form class="sign-page__form" action=" {{ route('register.store') }}" method="POST">
+                    @csrf
+                    <input type="text" name="name" class="text text--right" placeholder="نام و نام خانوادگی" />
+                    <input type="text" name="mobile" class="text text--left" placeholder="شماره موبایل" />
+                    <input type="text" name="email" class="text text--left" placeholder="ایمیل" />
+                    <input type="password" name="password" class="text text--left" placeholder="رمز عبور" />
+                    <input type="password" name="password_confirmation" class="text text--left" placeholder="تکرار رمز عبور" />
+    
+    
+                        <button type="submit" class="btn btn--blue btn--shadow-blue width-100 mb-10">ثبت نام</button>
+                        <button type="reset" class="btn btn--red btn--shadow-red width-100 ">پاک کردن</button>
+                        <div class="sign-page__footer">
+                            <span>در سایت عضوید ؟ </span>
+                            <a href="{{ route('login') }}" class="color--46b2f0">صفحه ورود</a>
+                        </div>
+                </form>
             </div>
-
-            <!-- Email Address -->
-            <div class="mt-4">
-                <x-label for="email" :value="__('Email')" />
-
-                <x-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required />
-            </div>
-
-            <!-- Password -->
-            <div class="mt-4">
-                <x-label for="password" :value="__('Password')" />
-
-                <x-input id="password" class="block mt-1 w-full"
-                                type="password"
-                                name="password"
-                                required autocomplete="new-password" />
-            </div>
-
-            <!-- Confirm Password -->
-            <div class="mt-4">
-                <x-label for="password_confirmation" :value="__('Confirm Password')" />
-
-                <x-input id="password_confirmation" class="block mt-1 w-full"
-                                type="password"
-                                name="password_confirmation" required />
-            </div>
-
-            <div class="flex items-center justify-end mt-4">
-                <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('login') }}">
-                    {{ __('Already registered?') }}
-                </a>
-
-                <x-button class="ml-4">
-                    {{ __('Register') }}
-                </x-button>
-            </div>
-        </form>
-    </x-auth-card>
-</x-guest-layout>
+        </div>
+    </main>
+    </x-app-layout>
