437112c60023a5a89f0d6cbd45bb7bcc601a56a0
Hadi-pd <h7ad3i@chmail.ir>
2021-12-18 05:45:02 +0330

    part 7

--- routes/web.php
+++ routes/web.php
@@ -4,4 +4,20 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('landing');
-});
+})->name('landing');
+
+Route::get('/login', function () {
+    return view('login');
+})->name('login');
+
+Route::get('/register', function () {
+    return view('register');
+})->name('register');
+
+Route::get('/reset-password', function () {
+    return view('reset-password');
+})->name('reset-pass');
+
+Route::get('/post/{id}', function ($id) {
+    return view('post');
+})->name('post.show');
