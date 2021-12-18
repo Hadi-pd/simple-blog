6dfb652971acc0e33cba8a538ec062830111895c
Hadi-pd <h7ad3i@chmail.ir>
2021-12-18 13:50:45 +0330

    Add breeze

--- routes/web.php
+++ routes/web.php
@@ -2,22 +2,26 @@
 
 use Illuminate\Support\Facades\Route;
 
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
 Route::get('/', function () {
     return view('landing');
 })->name('landing');
 
-Route::get('/login', function () {
-    return view('login');
-})->name('login');
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
 
-Route::get('/register', function () {
-    return view('register');
-})->name('register');
-
-Route::get('/reset-password', function () {
-    return view('reset-password');
-})->name('reset-pass');
-
-Route::get('/post/{id}', function ($id) {
+Route::get('/post/{id}',function($id){
     return view('post');
 })->name('post.show');
+require __DIR__.'/auth.php';
