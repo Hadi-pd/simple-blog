c6a6d6275298b84ed328258bbf756627e391b0c0
Hadi-pd <h7ad3i@chmail.ir>
2021-12-19 09:32:28 +0330

    Add Panel

--- routes/web.php
+++ routes/web.php
@@ -18,7 +18,7 @@ Route::get('/', function () {
 })->name('landing');
 
 Route::get('/dashboard', function () {
-    return view('dashboard');
+    return view('panel.index');
 })->middleware(['auth'])->name('dashboard');
 
 Route::get('/post/{id}',function($id){
