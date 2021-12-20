0d6fedbac7e5bae50fa2340c8a2efe6a38bb49f6
Hadi-pd <h7ad3i@chmail.ir>
2021-12-20 20:39:02 +0330

    Category Add

--- app/Http/Controllers/Panel/CategoryController.php
+++ app/Http/Controllers/Panel/CategoryController.php
@@ -10,12 +10,26 @@ class CategoryController extends Controller
 {
     public function index()
     {
-        return view('panel.categories.index');
+        $parentCategories = Category::where('category_id', null)->get();
+
+        return view('panel.categories.index', compact('parentCategories'));
     }
 
     public function store(Request $request)
     {
-        //
+        $request->validate([
+            'name' => ['required', 'string', 'max:255'],
+            'slug' => ['required', 'string', 'max:255', 'unique:categories'],
+            'category_id' => ['nullable', 'exists:categories,id']
+        ]);
+
+        Category::create(
+            $request->only(['name', 'slug', 'category_id'])
+        );
+
+        session()->flash('status', 'دسته بندی مد نظر به درستی ایجاد شد!');
+
+        return back();
     }
 
     public function update(Request $request, Category $category)
--- database/migrations/2021_12_20_152946_create_categories_table.php
+++ database/migrations/2021_12_20_152946_create_categories_table.php
@@ -16,7 +16,7 @@ class CreateCategoriesTable extends Migration
         Schema::create('categories', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('slug');
+            $table->string('slug')->unique();
             
             $table->unsignedBigInteger('category_id')->nullable();
             $table->foreign('category_id')
