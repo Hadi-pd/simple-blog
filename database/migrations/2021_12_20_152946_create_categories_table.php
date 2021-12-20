42f44af15814a6cac01139574458001066ce8535
Hadi-pd <h7ad3i@chmail.ir>
2021-12-20 19:25:43 +0330

    Categories migration

--- database/migrations/2021_12_20_152946_create_categories_table.php
+++ database/migrations/2021_12_20_152946_create_categories_table.php
@@ -15,6 +15,15 @@ class CreateCategoriesTable extends Migration
     {
         Schema::create('categories', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->string('slug');
+            
+            $table->unsignedBigInteger('category_id')->nullable();
+            $table->foreign('category_id')
+                    ->references('id')
+                    ->on('categories')
+                    ->onUpdate('CASCADE')
+                    ->onDelete('CASCADE');
             $table->timestamps();
         });
     }
