15ac8ac4b23992dd2b1e705b3172d689535d9254
Hadi-pd <h7ad3i@chmail.ir>
2021-12-21 15:14:36 +0330

    Code Refactor

--- app/Http/Controllers/Panel/CategoryController.php
+++ app/Http/Controllers/Panel/CategoryController.php
@@ -4,28 +4,23 @@ namespace App\Http\Controllers\Panel;
 
 use App\Http\Controllers\Controller;
 use App\Models\Category;
-use Illuminate\Http\Request;
+use App\Http\Requests\Panel\Category\CategoryCreateRequest;
+use App\Http\Requests\Panel\Category\CategoryUpdateRequest;
 
 class CategoryController extends Controller
 {
     public function index()
     {
-        $categories = Category::paginate();
+        $categories = Category::with('parent')->paginate();
         $parentCategories = Category::where('category_id', null)->get();
 
         return view('panel.categories.index', compact('categories', 'parentCategories'));
     }
 
-    public function store(Request $request)
+    public function store(CategoryCreateRequest $request)
     {
-        $request->validate([
-            'name' => ['required', 'string', 'max:255'],
-            'slug' => ['required', 'string', 'max:255', 'unique:categories'],
-            'category_id' => ['nullable', 'exists:categories,id']
-        ]);
-
         Category::create(
-            $request->only(['name', 'slug', 'category_id'])
+            $request->validated()
         );
 
         session()->flash('status', 'دسته بندی مد نظر به درستی ایجاد شد!');
@@ -40,14 +35,10 @@ class CategoryController extends Controller
         return view('panel.categories.edit', compact('category', 'parentCategories'));
     }
 
-    public function update(Request $request, Category $category)
+    public function update(CategoryUpdateRequest $request, Category $category)
     {
-        $request->validate([
-            'name' => ['required', 'string', 'max:255'],
-            'category_id' => ['nullable', 'exists:categories,id']
-        ]);
         $category->update(
-            $request->only(['name', 'category_id'])
+            $request->validated()
         );
 
         session()->flash('status', 'دسته بندی به درستی اپدیت شد!');
