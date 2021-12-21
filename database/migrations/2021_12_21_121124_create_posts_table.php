8dc25a5f0865b1d42361cccb0a2784dbeb66f9f4
Hadi-pd <h7ad3i@chmail.ir>
2021-12-21 20:39:39 +0330

    Post migrations

--- database/migrations/2021_12_21_121124_create_posts_table.php
+++ database/migrations/2021_12_21_121124_create_posts_table.php
@@ -15,6 +15,10 @@ class CreatePostsTable extends Migration
     {
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
+            $table->string('title');
+            $table->string('slug');
+            $table->string('banner');
+            $table->longText('content');
             $table->timestamps();
         });
     }
