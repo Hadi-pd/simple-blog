dd87dc861b82284bf6ff5310a005169b6fa4bab0
Hadi-pd <h7ad3i@chmail.ir>
2021-12-24 20:17:00 +0330

    Fix show comments approved

--- app/Http/Controllers/ShowPostController.php
+++ app/Http/Controllers/ShowPostController.php
@@ -10,7 +10,7 @@ class ShowPostController extends Controller
     public function show(Post $post)
     {
         $post->load(['user', 'categories', 'comments' => function($query) {
-            return $query->where('comment_id', null);
+            return $query->where('comment_id', null)->where('is_approved', true);
         }])->loadCount('comments');
 
         return view('post', compact('post'));
