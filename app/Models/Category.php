e531db492bcadde06702967bab019379480b5d4a
Hadi-pd <h7ad3i@chmail.ir>
2021-12-24 11:34:31 +0330

    Add categories to menu

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -20,6 +20,11 @@ class Category extends Model
         return $this->belongsTo(Category::class, 'category_id');
     }
 
+    public function children()
+    {
+        return $this->hasMany(Category::class, 'category_id');
+    }
+
     public function posts()
     {
         return $this->belongsToMany(Post::class);
