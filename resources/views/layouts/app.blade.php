40aa59b936d5ab812f59feb9d4bac98a6da535c3
Hadi-pd <h7ad3i@chmail.ir>
2021-12-26 16:48:45 +0330

    Add new mi style header & footer

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,181 +1,179 @@
-<!doctype html>
-<html lang="fa">
+<!DOCTYPE html>
+<html lang="en">
+
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0;">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet"
+        href="https://maxst.icons8.com/vue-static/landings/line-awesome/line-awesome/1.3.0/css/line-awesome.min.css">
+    <link rel="stylesheet" href="{{ asset('/blog/mi/font/fonts.css') }}">
+    <link rel="stylesheet" href="{{ asset('/blog/mi/css/style.css') }}">
 
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>وبلاگ {{ $title ?? '' }}</title>
-    <meta name="description"
-          content="وب آموز وبسایت آموزش برنامه نویسی وب و موبایل ، جاوااسکریپت ، لاراول ، react ، آموزش node js با مجرب ترین مدرسین">
-    <meta name="keywords"
-          content="آموزش طراحی سایت,آموزش برنامه نویسی,طراحی وب,ساخت وب سایت,آموزش git,آموزش لاراول,آموزش php,آموزش react,آموزش پی اچ پی,آموزش laravel,آموزش جاوا اسکریپت,آموزش ساخت وب سایت,آموزش mvc,آموزش React Native,وب آموز , وب اموز">
-    <link rel="canonical" href="https://webamooz.net"/>
-    <link rel="stylesheet" href="{{ asset('/blog/css/fonts.css') }}">
-    <link rel="stylesheet" href="{{ asset('/blog/css/style.css') }}">
-    <!-- <link rel="stylesheet" href="{{ asset('/blog/panel/css/style.css') }}"> -->
-    <link rel="stylesheet" href="{{ asset('/blog/css/responsive.css') }}" media="(max-width:991px)">
-    <!--    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/Swiper/3.4.1/css/swiper.min.css">-->
+    <title>Sign in</title>
 </head>
+
 <body>
-<header class="c-header">
-    <div class="container container--responsive container--white">
-        <div class="c-header__row ">
-            <div class="c-header__right">
-                <div class="logo">
-                    <a href="{{ route('landing') }}" class="logo__img"></a>
-                </div>
-                <div class="c-search width-100 ">
-                    <form action="{{ route('post.search') }}" class="c-search__form position-relative">
-                        <input type="text" name="search" class="c-search__input" placeholder="جستجو کنید" value="{{ request()->search ?? '' }}">
-                        <button class="c-search__button"></button>
-                    </form>
-                </div>
-
-            </div>
-            <div class="c-header__left">
-                <div class="c-header__icons">
-                    <div class="c-header__button-search "></div>
-                    <div class="c-header__button-nav"></div>
-                </div>
-                @guest
-                    <div class="c-button__login-regsiter">
-                        <div><a class="c-button__link c-button--login" href="{{ route('login') }}">ورود</a></div>
-                        <div><a class="c-button__link c-button--register" href="{{ route('register') }}">ثبت نام</a></div>
-                    </div>
-                @else
-                <div style="width: 180px;">
-                    <div class="dropdown-select wide" id="dropdown-user" onclick="toggleUserDropdown()" tabindex="0">
-                        <span class="current">
-                        {{ auth()->user()->name }}
-                        </span>
-                        <div class="list">
-                            <ul>
-                                <li class="option" data-value="0" data-display-text="" tabindex="0">
-                                    <a href="{{ route('profile') }}">پروفایل</a>
-                                </li>
-                                <li class="option " data-value="0" data-display-text="" tabindex="0" onclick="logoutUser()">
-                                    خروج
-                                </li>
-                            </ul>
-                            <form action="{{ route('logout') }}" method="POST" id="logout-form">
-                                @csrf
-                            </form>
-                        </div>
-                    </div>
-                </div>
-                @endguest
-            </div>
-        </div>
-    </div>
-    <nav class="nav" id="nav">
-        <div class="c-button__login-regsiter d-none">
-            <div><a class="c-button__link c-button--login" href="{{ route('login') }}">ورود</a></div>
-            <div><a class="c-button__link c-button--register" href="{{ route('register') }}">ثبت نام</a></div>
-        </div>
-        <div class="container container--nav">
-            <ul class="nav__ul">
-                <li class="nav__item"><a href="{{ route('landing') }}" class="nav__link">صفحه اصلی</a></li>
-                @foreach($categories as $category)
-                <li class="nav__item nav__item--has-sub"><a href="{{ route('category.show', $category->slug) }}" class="nav__link">{{ $category->name }}</a>
-                    <div class="nav__sub">
-                        <div class="container d-flex item-center flex-wrap container--nav">
-                            @foreach($category->children as $childCategory)
-                                <a href="{{ route('category.show', $childCategory->slug) }}" class="nav__link">{{ $childCategory->name }}</a>
-                            @endforeach
-                        </div>
-                    </div>
+    <section class="sticky-back sign-back colCenter">
+        <section class="ham-menu-container colCenter">
+            <ul class="ham-menu-ul">
+                <li><a href="#" onclick="hamCloseHandler()">
+                        <h4>مقالات تخصصی</h4>
+                    </a></li>
+                <li><a href="#" onclick="hamCloseHandler()">
+                        <h4>تماس با ما</h4>
+                    </a></li>
+                <li><a href="#" onclick="hamCloseHandler()">
+                        <h4>درباره ما</h4>
+                    </a></li>
+                <li class="license-wrapper">
+                    <a href="#">
+                        <img src="{{ asset('/blog/mi/img/brand_value-002.png') }}" alt="license">
+                    </a>
                 </li>
-                @endforeach
-                <li class="nav__item"><a href="#" class="nav__link">درباره ما</a></li>
-                <li class="nav__item"><a href="#" class="nav__link">تماس باما</a></li>
+                <li class="license-wrapper">
+                    <a href="#" onclick="hamCloseHandler()">
+                        <img src="{{ asset('/blog/mi/img/menu.png') }}" alt="license">
+                    </a>
+                </li>
+                <li class="license-wrapper">
+                    <a href="#" onclick="hamCloseHandler()">
+                        <img src="{{ asset('/blog/mi/img/jav5.png') }}" alt="license">
+                    </a>
+                </li>
+                <li class="license-wrapper">
+                    <a href="#" onclick="hamCloseHandler()">
+                        <img src="{{ asset('/blog/mi/img/jav4.png') }}" alt="license">
+                    </a>
+                </li>
+                <li class="license-wrapper">
+                    <a href="#" onclick="hamCloseHandler()">
+                        <img src="{{ asset('/blog/mi/img/jav3.png') }}" alt="license">
+                    </a>
+                </li>
+                <li class="license-wrapper">
+                    <a href="#" onclick="hamCloseHandler()">
+                        <img src="{{ asset('/blog/mi/img/jav2.png') }}" alt="license">
+                    </a>
+                </li>
+                <li class="license-wrapper">
+                    <a href="#" onclick="hamCloseHandler()">
+                        <img src="{{ asset('/blog/mi/img/jav1.png') }}" alt="license">
+                    </a>
+                </li>
             </ul>
-        </div>
-    </nav>
-</header>
+        </section>
+        <!-- Header -->
+        <header class="header-container rowBetCenter">
+            <i class="las la-bars" onclick="menuClickHandler(this)"></i>
+            <nav class="header-nav">
+                <ul>
+                    <li><a href="#">
+                            <h4>مقالات تخصصی</h4>
+                        </a></li>
+                    <li><a href="#">
+                            <h4>تماس با ما</h4>
+                        </a></li>
+                    <li><a href="#">
+                            <h4>درباره ما</h4>
+                        </a></li>
+                </ul>
+            </nav>
+            <ul class="license-container rowBetCenter">
+                <li class="license-wrapper">
+                    <a href="#">
+                        <img src="{{ asset('/blog/mi/img/brand_value-002.png') }}" alt="license">
+                    </a>
+                </li>
+                <li class="license-wrapper">
+                    <a href="#">
+                        <img src="{{ asset('/blog/mi/img/menu.png') }}" alt="license">
+                    </a>
+                </li>
+                <li class="license-wrapper">
+                    <a href="#">
+                        <img src="{{ asset('/blog/mi/img/jav5.png') }}" alt="license">
+                    </a>
+                </li>
+                <li class="license-wrapper">
+                    <a href="#">
+                        <img src="{{ asset('/blog/mi/img/jav4.png') }}" alt="license">
+                    </a>
+                </li>
+                <li class="license-wrapper">
+                    <a href="#">
+                        <img src="{{ asset('/blog/mi/img/jav3.png') }}" alt="license">
+                    </a>
+                </li>
+                <li class="license-wrapper">
+                    <a href="#">
+                        <img src="{{ asset('/blog/mi/img/jav2.png') }}" alt="license">
+                    </a>
+                </li>
+                <li class="license-wrapper">
+                    <a href="#">
+                        <img src="{{ asset('/blog/mi/img/jav1.png') }}" alt="license">
+                    </a>
+                </li>
+            </ul>
+            <div class="logo-wrapper">
+                <img src="{{ asset('/blog/mi/img/logotop.png') }}" alt="logo">
+            </div>
+        </header>
 
-{{ $slot }}
+        
+        {{ $slot }}
 
-<footer class="footer">
-    <a href="" class="scroll-top"></a>
+    </section>
+    <!-- Footer -->
+    <footer class="footer-container rowBetStart">
+        <section class="ul-link-container colStart">
+            <ul>
+                <li><a href="#">لورم ایپسوم متن ساختگی با تولید</a></li>
+                <li><a href="#">لورم ایپسوم متن ساختگی با تولید</a></li>
+                <li><a href="#">لورم ایپسوم متن ساختگی با تولید</a></li>
+                <li><a href="#">لورم ایپسوم متن ساختگی با تولید</a></li>
+                <li><a href="#">لورم ایپسوم متن ساختگی با تولید</a></li>
+            </ul>
+        </section>
+        <section class="footer-license-container colCenter">
+            <div class="footer-license-img rowBetCenter">
+                <div class="footer-license-imgs-wrapper colStart">
+                    <a href="#" class="lic1">
+                        <img src="{{ asset('/blog/mi/img/javaz1.png') }}" alt="javaz">
+                    </a>
+                    <a href="#" class="lic1">
+                        <img src="{{ asset('/blog/mi/img/javaz2.png') }}" alt="javaz">
+                    </a>
+                    <a href="#" class="lic1">
+                        <img src="{{ asset('/blog/mi/img/javaz3.png') }}" alt="javaz">
+                    </a>
+                </div>
+                <a href="#" class="footer-logo">
+                    <img src="{{ asset('/blog/mi/img/logo.png') }}" alt="logo">
+                </a>
+            </div>
+            <input type="text" name="search" id="input-srch" placeholder="جستجو کنید ...">
+        </section>
+        <p class="footer-p">لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است.
+            چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است و برای شرایط فعلی تکنولوژی مورد نیاز و
+            کاربردهای متنوع با هدف بهبود ابزارهای کاربردی می باشد. کتابهای زیادی در شصت و لخصوص طراحان خلاقی و فرهنگ
+            پیشرو در زبان فارسی ایجاد کرد. در این صورت می توان امید داشت که تمام</p>
+    </footer>
+    <script src="{{ asset('/blog/mi/js/Scripts.js') }}"></script>
+    <script src="{{ asset('/blog/js/jquery-3.4.1.min.js') }}"></script>
+    <script src="{{ asset('/blog/js/js.js') }}"></script>
+    <!--<script src="https://cdnjs.cloudflare.com/ajax/libs/Swiper/3.4.1/js/swiper.min.js"></script>-->
+    <script>
+        function toggleUserDropdown() {
+            document.getElementById('dropdown-user').classList.toggle('open')
+        }
 
-    <div class="container">
-        <div class="footer__links">
-            <a href="" class="footer__link">لینک اول</a>
-            <a href="" class="footer__link">لینک اول</a>
-            <a href="" class="footer__link">لینک اول</a>
-            <a href="" class="footer__link">لینک اول</a>
-            <a href="" class="footer__link">لینک اول</a>
-            <a href="" class="footer__link">لینک اول</a>
-        </div>
-        <div class="footer__hr"></div>
-        <div class="footer__about">
-            <p class="footer__txt">
-                وب اموز مرجعی تخصصی برای یادگیری طراحی و برنامه نویسی وب و موبایل است. ما در وب اموز با بهره گیری از
-                نیروهای متخصص، باتجربه تمام تلاش خود را برای تهیه و تولید آموزش های با کیفیت و کامل و حرفه ای انجام
-                می دهیم. باور ما اینست که کاربران ایرانی لایق بهترین ها هستند و باید بهترین و بروزترین فیلم های
-                آموزشی، در اختیار آنها قرار بگیرد تا بتوانند به سرعت پیشرفت کنند و جزء بهترین ها شوند. امید است که
-                با همراهی هر چه بیشتر شما کاربران عزیز در این راه، ما را برای حرکتی قدرتمند در مسیر این هدف باارزش
-                یاری دهید.
-            </p>
-        </div>
-    </div>
-    <div class="footer__webamooz">
-        طراحی و توسعه با لاراول توسط تیم
-        <a class="footer__copy" href="https://webamooz.net">وب آموز</a>
-        © 1399
-    </div>
-    <!--    <div class="footer-info color-444">-->
-    <!--        <a class="scrollToTop"></a>-->
-    <!--        <div class="container">-->
-    <!--            <div class="f-links">-->
-    <!--                <div class="col-2"><a href="">خدمات هاستینگ بستلا</a></div>-->
-    <!--                <div class="col-2"><a href="">لینک اول</a></div>-->
-    <!--                <div class="col-2"><a href="">لینک اول</a></div>-->
-    <!--                <div class="col-2"><a href="">لینک اول</a></div>-->
-    <!--                <div class="col-2"><a href="">لینک اول</a></div>-->
-    <!--                <div class="col-2"><a href="">لینک اول</a></div>-->
-    <!--            </div>-->
-    <!--        </div>-->
-    <!--        <div class="line"></div>-->
-    <!--        <div class="container">-->
-    <!--            <div class="f-about">-->
-    <!--                <div class="col-8 margin-bottom-15">-->
-    <!--                    <p>-->
-    <!--                        وب اموز مرجعی تخصصی برای یادگیری طراحی و برنامه نویسی وب و موبایل است. ما در وب اموز با بهره-->
-    <!--                        گیری از-->
-    <!--                        نیروهای متخصص، باتجربه تمام تلاش خود را برای تهیه و تولید آموزش های با کیفیت و کامل و حرفه ای-->
-    <!--                        انجام-->
-    <!--                        می دهیم. باور ما اینست که کاربران ایرانی لایق بهترین ها هستند و باید بهترین و بروزترین فیلم های-->
-    <!--                        آموزشی، در اختیار آنها قرار بگیرد تا بتوانند به سرعت پیشرفت کنند و جزء بهترین ها شوند. امید است-->
-    <!--                        که-->
-    <!--                        با همراهی هر چه بیشتر شما کاربران عزیز در این راه، ما را برای حرکتی قدرتمند در مسیر این هدف-->
-    <!--                        باارزش-->
-    <!--                        یاری دهید.-->
-    <!--                    </p>-->
-    <!--                </div>-->
-    <!--            </div>-->
-    <!--        </div>-->
-    <!--        <div class="webamooz">-->
-    <!--            طراحی و توسعه با لاراول توسط تیم-->
-    <!--            <a href="https://webamooz.net">وب آموز</a>-->
-    <!--            © 1399-->
-    <!--        </div>-->
-    <!--    </div>-->
-</footer>
-<div class="overlay"></div>
-<script src="{{ asset('/blog/js/jquery-3.4.1.min.js') }}"></script>
-<script src="{{ asset('/blog/js/js.js') }}"></script>
-<!--<script src="https://cdnjs.cloudflare.com/ajax/libs/Swiper/3.4.1/js/swiper.min.js"></script>-->
-<script>
-function toggleUserDropdown() {
-    document.getElementById('dropdown-user').classList.toggle('open')
-}
-function logoutUser() {
-    document.getElementById('logout-form').submit();
-}
-</script>
-{{ $scripts ?? '' }}
-
+        function logoutUser() {
+            document.getElementById('logout-form').submit();
+        }
+    </script>
+    {{ $scripts ?? '' }}
 </body>
+
 </html>
